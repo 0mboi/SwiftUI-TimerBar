119efe9ef9fbab3985f8295a925b98c0258dff03
Christopher Hicks <visrule@protonmail.com>
2021-01-28 13:06:41 -0800

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Christopher Hicks on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
